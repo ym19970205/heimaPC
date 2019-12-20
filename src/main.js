c1587749964e28607db8a4eb48129786aece38e1
Your Name <you@example.com>
2019-12-20 18:14:28 +0800

    黑马头条PC-统一全局注册插件的使用方式Vue.use

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ele from 'element-ui'
+import Component from './components'
 import axios from 'axios'
 import 'element-ui/lib/theme-chalk/index.css'
 import './styles/index.less'
+
 Vue.use(ele)
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 Vue.prototype.$axios = axios
+Vue.use(Component)
 
 Vue.config.productionTip = false
 
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,26 +1,32 @@
 <template>
+  <!-- 放置一个大容器 -->
   <el-container>
-    <el-aside style="height:100vh;background-color:#323745;width:230px">
-     <layoutAside></layoutAside>
+    <!-- 左右布局 -->
+    <el-aside style="height:100vh;background-color:#353b4e;width:230px;">
+      <layout-aside></layout-aside>
     </el-aside>
+    <!-- 右侧容器 -->
     <el-container>
+      <!-- 上下布局 -->
       <el-header>
-       <layoutHeader></layoutHeader>
+        <!-- 头部组件 -->
+        <layout-header></layout-header>
       </el-header>
       <el-main>
-        内容
+         <!-- 二级路由容器 -->
+         <router-view></router-view>
       </el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
-import layoutAside from '../../components/home/layout-aside'
-import layoutHeader from '../../components/home/layout-header'
+
 export default {
-  components: {
-    layoutAside,
-    layoutHeader
-  }
+
 }
 </script>
+
+<style>
+
+</style>
