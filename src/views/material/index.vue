f29829a6ac641bcb243782c42f4a5744ebd20a7b
Your Name <you@example.com>
2019-12-23 17:42:35 +0800

    黑马头条PC-素材列表-分页组件及请求

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -16,6 +16,7 @@
           </el-row>
         </el-card>
       </div>
+
     </el-tab-pane>
     <el-tab-pane label="收藏" name="sc">
       <div class="img-list">
@@ -25,6 +26,18 @@
       </div>
     </el-tab-pane>
   </el-tabs>
+  <!-- 公共的分页组件-->
+      <el-row type="flex" justify="center">
+        <el-pagination
+          background
+          layout="prev, pager, next"
+          :total="page.total"
+          :current-page="page.currentPage"
+          :page-size="page.pageSize"
+          @current-change="changePage"
+          >
+        </el-pagination>
+      </el-row>
 </el-card>
 </template>
 
@@ -33,22 +46,41 @@ export default {
   data () {
     return {
       activeName: 'all',
-      list: [] // 定义一个接受的数组
+      list: [], // 定义一个接受的数组
+      page: {
+        currentPage: 1,
+        pageSize: 8,
+        total: 0
+
+      }
     }
   },
   methods: {
+    // 改变页码的方法
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.getMaterial()
+    },
+    // 切换页签的方法
     changeTab () {
+      this.page.currentPage = 1 // 切换页签时，我们需要把组件中的页码切换到第一页
       this.getMaterial() // 当点击这个改变事件的时候，触发收藏与全部布尔值的判断
     },
+    // 获取素材的方法
     getMaterial () {
       this.$axios({
         url: '/user/images',
         params: {
           // collect: false // 传入false是获取所有的数据，传入true是获取收藏的数据
-          collect: this.activeName === 'sc'
+          collect: this.activeName === 'sc',
+          page: this.page.currentPage,
+          per_page: this.page.pageSize
         }
       }).then((res) => {
         this.list = res.data.results // 获取图片信息
+        this.page.total = res.data.total_count
+        // this.page.currentPage = res.data.page
+        // this.page.pageSize = res.data.per_page
       })
     }
   },
