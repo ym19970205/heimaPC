1766768360484210f9f055b216cdcc3c3d718943
Your Name <you@example.com>
2019-12-26 19:01:25 +0800

    黑马头条PC-发布文章-封面功能设计

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -23,6 +23,7 @@
             <el-radio :label="0">无图</el-radio>
             <el-radio :label="-1">自动</el-radio>
           </el-radio-group>
+          {{formData.cover}}
           </el-form-item>
           <el-form-item label="频道" prop="channel_id">
               <el-select v-model="formData.channel_id" placeholder="请选择">
@@ -83,6 +84,15 @@ export default {
           },
           channel_id: null
         }
+      }
+    },
+    'formData.cover.type': function () {
+      if (this.formData.cover.type === 0 || this.formData.cover.type === -1) {
+        this.formData.cover.images = [] // 无图或者自动
+      } else if (this.formData.cover.type === 1) {
+        this.formData.cover.images = ['']
+      } else if (this.formData.cover.type === 3) {
+        this.formData.cover.images = ['', '', '']
       }
     }
   },
