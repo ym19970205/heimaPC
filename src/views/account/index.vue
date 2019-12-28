9d8b624b240541f0fe0cb498a37c537bf1803e57
Your Name <you@example.com>
2019-12-28 09:48:37 +0800

    黑马头条PC-账户信息-数据加载给表单

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -6,33 +6,62 @@
       </template>
       </bread-crumb>
     <!-- 表单 => 表单容器 -->
-     <el-form label-width="80px" style="margin-left:50px">
-       <el-form-item label="用户名">
-         <el-input style="width:40%"></el-input>
+     <el-form :model="formData" :rules="rules" ref="ruleForm" label-width="80px" style="margin-left:50px">
+       <el-form-item label="用户名" prop="name">
+         <el-input v-model="formData.name" style="width:40%"></el-input>
        </el-form-item>
-       <el-form-item label="简介">
-         <el-input style="width:40%"></el-input>
+       <el-form-item label="简介" prop="intro">
+         <el-input v-model="formData.intro" style="width:40%"></el-input>
        </el-form-item>
-       <el-form-item label="邮箱">
-         <el-input style="width:40%"></el-input>
+       <el-form-item label="邮箱" prop="email">
+         <el-input v-model="formData.email" style="width:40%"></el-input>
        </el-form-item>
        <el-form-item label="手机号">
-         <el-input style="width:40%" disabled=""></el-input>
+         <el-input v-model="formData.mobile" style="width:40%" disabled="" ></el-input>
        </el-form-item>
        <el-form-item>
          <el-button type="primary">保存信息</el-button>
        </el-form-item>
      </el-form>
+
      <!--上传组件-->
-      <el-upload class="upload-img" action="" :show-file-list="false">
-        <img src="../../assets/img/default.jpg" alt="">
+      <el-upload prop="photo" class="upload-img" action="" :show-file-list="false">
+        <img :src="formData.photo?formData.photo:defalutImg" alt="">
       </el-upload>
   </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      formData: {
+        name: '', // 用户名
+        intro: '', // 简介
+        photo: '', // 头像
+        email: '', // 邮箱
+        mobile: '' // 手机号
 
+      },
+      defalutImg: require('../../assets/img/default.jpg'),
+      rules: {
+
+      }
+    }
+  },
+  methods: {
+    // 获取用户个人信息
+    getUserinfo () {
+      this.$axios({
+        url: '/user/profile'
+      }).then(res => {
+        this.formData = res.data
+      })
+    }
+  },
+  created () {
+    this.getUserinfo()
+  }
 }
 </script>
 
