db6372ff3734977bb044593ab7ca9154f15b5ace
Your Name <you@example.com>
2019-12-28 10:20:42 +0800

    黑马头条PC-账户信息-用户数据保存

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -6,7 +6,7 @@
       </template>
       </bread-crumb>
     <!-- 表单 => 表单容器 -->
-     <el-form :model="formData" :rules="rules" ref="ruleForm" label-width="80px" style="margin-left:50px">
+     <el-form :model="formData" :rules="rules" ref="myForm" label-width="80px" style="margin-left:50px">
        <el-form-item label="用户名" prop="name">
          <el-input v-model="formData.name" style="width:40%"></el-input>
        </el-form-item>
@@ -20,7 +20,7 @@
          <el-input v-model="formData.mobile" style="width:40%" disabled="" ></el-input>
        </el-form-item>
        <el-form-item>
-         <el-button type="primary">保存信息</el-button>
+         <el-button @click="saveInfo" type="primary">保存信息</el-button>
        </el-form-item>
      </el-form>
 
@@ -45,11 +45,32 @@ export default {
       },
       defalutImg: require('../../assets/img/default.jpg'),
       rules: {
+        name: [{ required: true, message: '用户名不能为空' },
+          { min: 1, max: 7, message: '用户名应该在1-7个字符之间' }],
+        email: [{ required: true, message: '邮箱不能为空' }, {
+          pattern: /^(\w-*\.*)+@(\w-?)+(\.\w{2,})+$/,
+          message: '邮箱格式不正确'
+        }]
 
       }
     }
   },
   methods: {
+    // 保存信息
+    saveInfo () {
+      this.$refs.myForm.validate().then(res => {
+        this.$axios({
+          url: '/user/profile',
+          method: 'patch',
+          data: this.formData
+        }).then(res => {
+          this.$message({
+            type: 'success',
+            message: '保存成功'
+          })
+        })
+      })
+    },
     // 获取用户个人信息
     getUserinfo () {
       this.$axios({
